d1f49bfa1ca345dc756f7ca1557eca081bac4d2d
Calvin Tandra <calvin.td43@gmail.com>
2019-10-27 20:18:09 +0700

    tinkering with icons and tests

--- app/src/test/java/com/calvintd/kade/soccerbase/ApiUnitTest.kt
+++ app/src/test/java/com/calvintd/kade/soccerbase/ApiUnitTest.kt
@@ -2,13 +2,12 @@ package com.calvintd.kade.soccerbase
 
 import com.calvintd.kade.soccerbase.api.RetrofitInstance
 import com.calvintd.kade.soccerbase.api.RetrofitService
-import com.calvintd.kade.soccerbase.itemmodel.LeagueResponse
+import com.calvintd.kade.soccerbase.itemmodel.Match
+import com.calvintd.kade.soccerbase.itemmodel.MatchLeagueResponse
 import com.calvintd.kade.soccerbase.itemmodel.MatchResponse
-import com.calvintd.kade.soccerbase.presenter.LeagueListingPresenter
 import com.calvintd.kade.soccerbase.presenter.LeagueSchedulePastMatchesPresenter
 import com.calvintd.kade.soccerbase.presenter.LeagueScheduleUpcomingMatchesPresenter
 import com.calvintd.kade.soccerbase.presenter.MatchSearchPresenter
-import com.calvintd.kade.soccerbase.view.LeagueListingView
 import com.calvintd.kade.soccerbase.view.LeagueScheduleView
 import com.calvintd.kade.soccerbase.view.MatchSearchView
 import kotlinx.coroutines.runBlocking
@@ -16,7 +15,7 @@ import org.junit.Test
 
 import org.junit.Before
 import org.mockito.Mock
-import org.mockito.Mockito.verify
+import org.mockito.Mockito.*
 import org.mockito.MockitoAnnotations
 import retrofit2.HttpException
 import retrofit2.Response
@@ -27,57 +26,54 @@ import retrofit2.Response
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ApiUnitTest {
-    @Mock
+    private val leagueId = 4328
+    private val leagueName = "English Premier League"
+
     private lateinit var service: RetrofitService
 
-    @Mock
-    private lateinit var leagueListingPresenter: LeagueListingPresenter
-    @Mock
     private lateinit var leagueSchedulePastMatchesPresenter: LeagueSchedulePastMatchesPresenter
-    @Mock
     private lateinit var leagueScheduleUpcomingMatchesPresenter: LeagueScheduleUpcomingMatchesPresenter
-    @Mock
     private lateinit var matchSearchPresenter: MatchSearchPresenter
 
-    @Mock
-    private lateinit var leagueListingView: LeagueListingView
-    @Mock
     private lateinit var leagueScheduleView: LeagueScheduleView
-    @Mock
     private lateinit var matchSearchView: MatchSearchView
 
     @Mock
-    private lateinit var leagueResponse: Response<LeagueResponse>
-    @Mock
     private lateinit var matchResponse: Response<MatchResponse>
+    @Mock
+    private lateinit var matchLeagueResponse: Response<MatchLeagueResponse>
 
     @Before
     fun setUp() {
+        service = mock(RetrofitService::class.java)
+
+        leagueSchedulePastMatchesPresenter = mock(LeagueSchedulePastMatchesPresenter::class.java)
+        leagueScheduleUpcomingMatchesPresenter = mock(LeagueScheduleUpcomingMatchesPresenter::class.java)
+        matchSearchPresenter = mock(MatchSearchPresenter::class.java)
+
+        leagueScheduleView = mock(LeagueScheduleView::class.java)
+        matchSearchView = mock(MatchSearchView::class.java)
+
         MockitoAnnotations.initMocks(this)
-
-        service = RetrofitInstance.getInstance()
-        leagueListingPresenter = LeagueListingPresenter(leagueListingView)
-        leagueSchedulePastMatchesPresenter = LeagueSchedulePastMatchesPresenter(leagueScheduleView)
-        leagueScheduleUpcomingMatchesPresenter = LeagueScheduleUpcomingMatchesPresenter(leagueScheduleView)
-        matchSearchPresenter = MatchSearchPresenter(matchSearchView)
-    }
-
-    @Test
-    fun loadLeagueListingTest() {
-        runBlocking {
-            verify(leagueListingView).loadData(leagueListingPresenter.getFetchedLeagues())
-            verify(leagueListingView).showResponseError(
-                leagueResponse.code(),
-                leagueResponse.errorBody()
-            )
-            verify(leagueListingView).showException(HttpException(leagueResponse))
-        }
     }
 
     @Test
     fun loadLeagueSchedulePastMatchesTest() {
-        runBlocking {
+        val leaguesList = mutableListOf<Match>()
 
+        runBlocking {
+            `when`(service.getPastLeagueMatches(leagueId)).thenReturn(matchLeagueResponse)
+            `when`(leagueSchedulePastMatchesPresenter.getFetchedMatches(leagueId)).thenReturn(leaguesList)
+
+            leagueSchedulePastMatchesPresenter.loadMatchesByLeague(leagueId, leagueName)
+
+            verify(leagueScheduleView).loadMatchesByLeague(leaguesList, leagueName)
+            verify(leagueScheduleView).showNoResultsFound(leagueName)
+            verify(leagueScheduleView).showResponseError(
+                matchLeagueResponse.code(),
+                matchLeagueResponse.errorBody()
+            )
+            verify(leagueScheduleView).showException(HttpException(matchLeagueResponse))
         }
     }
 
