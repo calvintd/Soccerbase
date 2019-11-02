59a562a0dc7d05f09a2c72051ff6c973d8694441
Calvin Tandra <calvin.td43@gmail.com>
2019-11-02 22:40:03 +0700

    preparing player and team adapters

--- app/src/main/java/com/calvintd/kade/soccerbase/adapter/PlayerAdapter.kt
+++ app/src/main/java/com/calvintd/kade/soccerbase/adapter/PlayerAdapter.kt
@@ -1,6 +1,38 @@
 package com.calvintd.kade.soccerbase.adapter
 
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.calvintd.kade.soccerbase.itemmodel.Player
+import org.jetbrains.anko.AnkoComponent
+import org.jetbrains.anko.AnkoContext
 
-class PlayerAdapter (private val players: List<Player>) {
+class PlayerAdapter (private val players: List<Player>) : RecyclerView.Adapter<PlayerAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ViewHolder(PlayerUI().createView(AnkoContext.Companion.create(parent.context, parent)))
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) =
+        holder.bindItem(players[position])
+
+    override fun getItemCount(): Int = players.size
+
+    class PlayerUI : AnkoComponent<ViewGroup> {
+        companion object {
+            const val iconSize = 40
+            const val badgeSize = 160
+            const val iconPadding = 6
+            const val buttonTextSize = 12f
+        }
+
+        override fun createView(ui: AnkoContext<ViewGroup>) = with(ui) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
+
+    class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+
+        fun bindItem (player: Player) {
+
+        }
+    }
 }
--- app/src/main/java/com/calvintd/kade/soccerbase/adapter/TeamAdapter.kt
+++ app/src/main/java/com/calvintd/kade/soccerbase/adapter/TeamAdapter.kt
@@ -1,6 +1,42 @@
 package com.calvintd.kade.soccerbase.adapter
 
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.calvintd.kade.soccerbase.itemmodel.Team
+import org.jetbrains.anko.AnkoComponent
+import org.jetbrains.anko.AnkoContext
 
-class TeamAdapter (private val teams: List<Team>) {
+class TeamAdapter (private val teams: List<Team>, private val detailsListener: (Team) -> Unit,
+                   private val playersListener: (Team) -> Unit) :
+    RecyclerView.Adapter<TeamAdapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ViewHolder(TeamUI().createView(AnkoContext.Companion.create(parent.context, parent)))
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bindItem(teams[position], detailsListener, playersListener)
+
+    override fun getItemCount(): Int = teams.size
+
+    class TeamUI : AnkoComponent<ViewGroup> {
+        companion object {
+            const val iconSize = 40
+            const val badgeSize = 160
+            const val iconPadding = 6
+            const val buttonTextSize = 12f
+        }
+
+        override fun createView(ui: AnkoContext<ViewGroup>) = with(ui) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
+
+    class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+
+
+        fun bindItem (team: Team, detailsListener: (Team) -> Unit, playersListener: (Team) -> Unit) {
+
+
+        }
+    }
 }
